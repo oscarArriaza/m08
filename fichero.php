2bbd3b112c49544ac293ded24e49802f3904ab82
oscarArriaza <arriaza22598@vk.com>
2024-01-30 16:09:56 +0100

    Primer cambio a ramaUno

--- fichero.php
+++ fichero.php
@@ -1,1 +1,12 @@
-<h1><?php echo "Hola Mundo con PHP";?></h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo "Hola Mundo con PHP";?></h1>
+    <h1>Esto es un cambio en la ramaUno</h1>
+</body>
+</html>
